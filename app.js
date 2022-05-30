ff0e3fb9f313ccdb80f45779319c3e0faae46e6e
AshMagill <ashmagillnz@gmail.com>
2022-05-30 15:56:35 +1200

    just started

---
+++ app.js
@@ -0,0 +1,18 @@
+// import express
+const express = require('express');
+
+//create react app
+const app = express();
+
+// import routes for form and viewing names
+const nameInput = require('./routes/nameInput')
+const nameList = require('./routes/nameList')
+
+//make a route '/' for name input
+app.use () =
+
+//make a route '/users' to view names
+
+
+
+
